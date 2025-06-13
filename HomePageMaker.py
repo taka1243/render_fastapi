dc5ab1218e40895052c88991f6212ce73dfb7100
taka1243 <takamz2004@gmail.com>
2025-06-13 12:06:12 +0900

    oooo

---
+++ HomePageMaker.py
@@ -0,0 +1,17 @@
+from fastapi.responses import HTMLResponse #インポート
+
+### コードいろいろ... ###
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
